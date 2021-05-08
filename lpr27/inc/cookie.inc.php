07af6fe35f1ca89b998e22e89960ca7d5e6500f5
coldsinurpillow <rainbeforedeath@mail.ru>
2021-05-09 02:27:03 +0600

    update lpr27

--- lpr27/inc/cookie.inc.php
+++ lpr27/inc/cookie.inc.php
@@ -6,7 +6,7 @@ if(date('d-m-Y', $_COOKIE['lastVisit']) != date('d-m-Y')) {
         $visitCounter = (int)$_COOKIE["visitCounter"];
         $visitCounter += 1;
     }
-    if(isset($_COOKIE["lastVisit"])) {
-        $lastVisit = date("Y-m-d H:i:s", $_COOKIE["lastVisit"]);
-    } 
 }
+if(isset($_COOKIE["lastVisit"])) {
+    $lastVisit = date("Y-m-d H:i:s", $_COOKIE["lastVisit"]);
+} 
